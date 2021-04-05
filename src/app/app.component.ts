7ed1085a48e1aca828d031a137d4f83b47e7f057
Milind Yadav <yadavmilind08@gmail.com>
2021-04-05 19:49:56 +0530

    Reseting form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,10 +44,13 @@ export class AppComponent {
 
   onSubmit() {
     this.submitted = true;
+
     this.user.username = this.signupForm.value.userData.username;
     this.user.email = this.signupForm.value.userData.email;
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.answer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
